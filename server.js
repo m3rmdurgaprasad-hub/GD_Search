40e3b352e053276ac24d96cfe4fe4b0ed18eab7a
m3rmdurgaprasad-hub <m3.rmdurgaprasad@gmail.com>
2025-10-27 19:04:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const BING_API_KEY = '34361b191bfa7b0470ff7093e33564ad';
 const BING_ENDPOINT = 'https://api.bing.microsoft.com/v7.0/search';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`gdSearch running on port ${PORT}`);
+});
+
